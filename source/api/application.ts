6c39a8e97ea47d3f7391068997fecb14308d1b6f
Bryan F <bryan.furia@gmail.com>
2017-07-01 11:33:26 -0400

    Include API source files

---
+++ source/api/application.ts
@@ -0,0 +1,70 @@
+import { Db } from "mongodb";
+import { Client, TextChannel, Message } from "discord.js";
+import { Router } from "express";
+import * as express from "express";
+
+import { catchAsync } from "../utils";
+
+export function application (db: Db, client: Client): Router {
+    const app = express();
+    const router = express.Router();
+    const apps = db.collection("applications");
+    const channel = <TextChannel>client.channels.find("id", "330582495164628992");
+
+    router.get("/get", catchAsync(async (req, res) => {
+        const doc = await apps.findOne({ "id": req.query.id });
+        if (doc)
+            res.json(doc);
+    }));
+
+    // create todo and send back all todos after creation
+    router.post("/submit", catchAsync(async (req, res) => {
+        const doc = await apps.findOne({ "id": req.body.id });
+        if (doc) {
+            const message = channel.messages.find("id", doc.messageid);
+            if (message) {
+                message.edit(messageBuilder(req));
+                req.body.messageid = doc.messageid;
+                await apps.updateOne({ "id": req.body.id },
+                    req.body,
+                    { upsert: true }
+                );
+                return;
+            }
+        }
+        const message = await channel.send(messageBuilder(req));
+        req.body.messageid = (<Message>message).id;
+        apps.updateOne({ "id": req.body.id },
+            req.body,
+            { upsert: true }
+        );
+    }));
+
+    // create todo and send back all todos after creation
+    router.post("/save", catchAsync(async (req, res) => {
+        apps.updateOne({ "id": req.body.id },
+            req.body,
+            { upsert: true }
+        );
+    }));
+
+    function messageBuilder(req): string {
+        return "**What is your GW2 ingame name (Example.1234)?**\n" +
+            req.body.gw2name +
+            "\n**We raid Sun/Mon/Weds 9:30 EST, are you available all these times?**\n" +
+            req.body.attendance +
+            "\n**Will you actively use your mic to contribute to the guild during raids and other times?**\n" +
+            req.body.mic +
+            "\n**What is/are your main classes?**\n" +
+            req.body.class +
+            "\n**What is your prior experience in wvw guilds?**\n" +
+            req.body.experience +
+            "\n**Show us the build you are currently running in WvW?**\n" +
+            req.body.build +
+            "\n**Do you have footage/can you record?**\n" +
+            req.body.record +
+            "\n**What's your capacity right now?**\n" +
+            req.body.capacity;
+    }
+    return router;
+}
---
+++ source/api/discordauth.ts
@@ -0,0 +1,67 @@
+import { Router } from "express";
+import * as express from "express";
+import * as fetch from "node-fetch";
+import * as btoa from "btoa";
+import * as cookieSession from "cookie-session";
+
+import * as config from "../config";
+import { catchAsync } from "../utils";
+
+
+export function discordauth (): Router {
+    const app = express();
+
+    const router = express.Router();
+
+    const CLIENT_ID = config.CLIENT_ID;
+    const CLIENT_SECRET = config.CLIENT_SECRET;
+    const redirect = config.redirect;
+
+    router.use(cookieSession({
+        name: "session",
+        keys: [config.token],
+        // Cookie Options
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }));
+
+    router.get("/login", (req, res) => {
+        res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
+    });
+
+    router.get("/logout", catchAsync(async (req, res) => {
+        req.session = null;
+        res.redirect(`/`);
+    }));
+
+    router.get("/callback", catchAsync(async (req, res) => {
+        if (!req.query.code) throw new Error("NoCodeProvided");
+        const code = req.query.code;
+        const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+        const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
+            {
+                method: "POST",
+                headers: {
+                    Authorization: `Basic ${creds}`,
+                },
+            });
+        const json = await response.json();
+        req.session.discordtoken = json.access_token;
+        res.redirect(`/`);
+    }));
+
+    router.get("/user", catchAsync(async (req, res) => {
+        console.log(req.session.discordtoken);
+        const response = await fetch("https://discordapp.com/api/users/@me",
+            {
+                method: "GET",
+                headers: {
+                    Authorization: "Bearer " + req.session.discordtoken
+                },
+            });
+        const json = await response.json();
+        console.log(json);
+        res.json(json);
+    }));
+
+    return router;
+}
